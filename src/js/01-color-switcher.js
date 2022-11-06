619c7392b60ec5ba7ce4d9d4cf8a798c34d52671
Zuza <zusapphire@gmail.com>
2022-11-06 12:59:16 +0100

    Task 1 commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start');
+const stopBtn = document.querySelector('[data-stop');
 
+let interval;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeColor = () => {
+    let color = getRandomHexColor();
+    document.querySelector('body').style.backgroundColor = color;
+};
+
+const start = () => {
+    startBtn.disabled = true;
+    interval = setInterval(changeColor, 1000)
+};
+
+const stop = () => {
+    clearInterval(interval);
+    startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);

70e56ff740d9da2508d82c25569fd8b380ade2d0
Zuza29 <zusapphire@gmail.com>
2022-11-10 21:07:01 +0000

    Deploying to gh-pages from @ Zuza29/test-repo@08d5c820db9a5beefb9696bc1e73fe866e4117c8 🚀

---
+++ 03-promises.c3138d08.js
@@ -0,0 +1,2 @@
+document.querySelector('[name="delay"]').value,document.querySelector('[name="step"]').value,document.querySelector('[name="amount"]').value;document.querySelector("button").addEventListener("click",(position,void Math.random()));
+//# sourceMappingURL=03-promises.c3138d08.js.map
---
+++ 03-promises.fa8bda1b.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector('[name="delay"]').value,document.querySelector('[name="step"]').value,document.querySelector('[name="amount"]').value;document.querySelector("button").addEventListener("click",(position,void Math.random()))}();
+//# sourceMappingURL=03-promises.fa8bda1b.js.map

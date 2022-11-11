50e44105b02761c07904186a4df9382c6700577d
Zuza <zusapphire@gmail.com>
2022-11-11 12:52:00 +0100

    Task 3 commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix, { Notify } from 'notiflix';
+
+let delay = Number(document.querySelector('input[name="delay"]').value);
+let step = Number(document.querySelector('input[name="step"]').value);
+let amount = Number(document.querySelector('input[name="amount"]').value);
+const submit = document.querySelector('button');
+
+
+const createPromise = (position, delay) => {
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const submitHandler = (event) => {
+  event.preventDefault();
+  for (let i = 0; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
   }
 };
 
-const delay = document.querySelector('[name="delay"]').value;
-const step = document.querySelector('[name="step"]').value;
-const amount = document.querySelector('[name="amount"]').value;
-const submit = document.querySelector('button');
-
-submit.addEventListener('click', createPromise(position, delay))
+submit.addEventListener('submit', submitHandler);
